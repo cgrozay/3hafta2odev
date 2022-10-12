e4dab2b748b5bc665be462d229845c16f1282f88
çağrı özay <cagri.ozay@outlook.com>
2022-10-12 13:34:53 +0300

    inheritance commit

---
+++ inheritance/src/inheritance/Customer.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class Customer extends Person {
+
+	
+	private String mail;
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+}
