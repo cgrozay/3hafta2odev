9ae9643579fd56deb6d9bca7dbbbcdfe4fbca68d
çağrı özay <cagri.ozay@outlook.com>
2022-10-12 13:34:14 +0300

    methodoverloading

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem=new DortIslem();
+		int total=dortIslem.topla(2,7,9);
+		System.out.println(total);
+
+	}
+
+}
